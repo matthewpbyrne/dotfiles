2047ad398f337aa4273fff1905ac358407476a79
Matthew Byrne <matthewpbyrne@gmail.com>
2024-02-25 23:24:48 +0000

    feat(zsh): .zsh/{antigen,fasd,quotes}.sh

---
+++ dot_zsh/antigen.zsh
@@ -0,0 +1,36 @@
+source $HOME/antigen.zsh
+
+# Load the oh-my-zsh's library.
+antigen use oh-my-zsh
+
+# Bundles from the default repo (robbyrussell's oh-my-zsh).
+antigen bundle asdf
+antigen bundle fasd
+antigen bundle fzf
+antigen bundle git
+antigen bundle heroku
+antigen bundle pip
+antigen bundle lein
+antigen bundle command-not-found
+
+# Syntax highlighting bundle.
+antigen bundle zsh-users/zsh-syntax-highlighting
+
+# Load the theme.
+# antigen theme robbyrussell
+# antigen theme amuse
+antigen theme jonathan
+
+# Tell Antigen that you're done.
+antigen apply
+
+# source $HOME/antigen.zsh
+# # Load the oh-my-zsh's library.
+# antigen use oh-my-zsh
+# antigen bundle git
+# # Syntax highlighting bundle.
+# antigen bundle zsh-users/zsh-syntax-highlighting
+# # Load the theme.
+# antigen theme robbyrussell
+# # Tell Antigen that you're done.
+# antigen apply
