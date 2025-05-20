d7f4eb90456693b9976d0b251a7098159e0b8f6b
Matthew Byrne <matthewpbyrne@gmail.com>
2025-05-20 11:04:16 +0100

    Revert "feat(doomemacs): --wip-- tuxedo hacking"
    This reverts commit de657c4bb233e6d811ec803f3854d3d462402e30.

--- dot_config/doom/packages.el
+++ dot_config/doom/packages.el
@@ -9,10 +9,6 @@
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;; (package! some-package)
 
-(package! org-super-agenda)
-; (package! org-ql)
-(package! org-ql :recipe (:host github :repo "alphapapa/org-ql"))
-
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/radian-software/straight.el#the-recipe-format
