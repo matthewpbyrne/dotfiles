b370f9d41688b1cff1f17550a4a48e6df447679e
Matthew Byrne <matthew@matthewpbyrne.com>
2025-05-20 21:36:58 +0100

    feat(doomemacs): enable TODO checkbox blocking

--- dot_config/doom/config.el
+++ dot_config/doom/config.el
@@ -41,6 +41,7 @@
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/"
+      org-enforce-todo-checkbox-dependencies t
       org-log-done 'time)
 
 
