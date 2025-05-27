4f61a7f5617f683585dabd5683d7f562cfab6457
Matthew Byrne <matthew@matthewpbyrne.com>
2025-05-27 15:24:17 +0100

    feat(doomemacs): --wip-- super agenda w org-ql

--- dot_config/doom/org/agenda.el
+++ dot_config/doom/org/agenda.el
@@ -18,7 +18,60 @@
                  (org-agenda-overriding-header "NEXT:")))
           (todo "CANCELLED"
                 ((org-agenda-todo-keyword-format "")
-                 (org-agenda-overriding-header "CANCELLED:")))))))
+                 (org-agenda-overriding-header "CANCELLED:")))))
+        ;; ))
 
+        ("Q" "⚡ Org-QL with Super Agenda"
+         ((org-ql-block
+           '(and (not (done))
+                 (todo)
+                 (tags "@work" "@home" "@errands"))
+           ((org-ql-block-header "⚡ Active Tasks")
+            (org-super-agenda-groups
+             '((:name "🧠 Work"
+                :tag "@work")
+               (:name "🏡 Home"
+                :tag "@home")
+               (:name "🛒 Errands"
+                :tag "@errands")
+               (:name "Waiting"
+                :todo "WAIT")
+               (:discard (:tag "someday"))))))))
 
+        ("C" "Compound GTD View !!!"
+         ((agenda ""
+                  ((org-agenda-span 1)
+                   (org-agenda-overriding-header "🕒 Today’s Schedule")))
 
+          (tags-todo "*"
+                     ((org-agenda-overriding-header "🏷 Contexts")
+                      (org-super-agenda-groups
+                       '((:name "Work"    :tag "@work")
+                         (:name "Home"    :tag "@home")
+                         (:name "Errands" :tag "@errands")
+                         (:discard (:tag "ben_button"))))))
+
+          (alltodo ""
+                   ((org-agenda-overriding-header "📌 Category grouping")
+                    (org-super-agenda-groups
+                     '((:name "Inbox"       :category "inbox")
+                       (:name "Projects"    :category "projects")
+                       (:discard (:category "States"))))))
+
+          (alltodo ""
+                   ((org-agenda-overriding-header "📌 Client grouping")
+                    (org-super-agenda-groups
+                     '((:auto-property "Client")
+                       (:discard (:anything))))))
+
+          (alltodo ""
+                   ((org-agenda-overriding-header "📌 Task Summary !!!")
+                    (org-super-agenda-groups
+                     '(
+                       (:name "Quick Wins"   :effort< "0:15")
+                       (:name "Deep Work"    :effort> "0:30")
+                       (:name "Waiting On"   :todo "WAIT")
+                       (:name "Next Actions" :todo "NEXT")
+                       (:name "Priority H"    :priority>= "B" :todo ("NEXT" "TODO" "IDEA"))
+                       (:name "Priority L"    :priority<= "B")
+                       (:discard (:tag "someday"))))))))))
