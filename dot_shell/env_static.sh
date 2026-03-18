e031d6370d9b8ab080a122bcb610fdbc8840c357
Matthew Byrne <matthewpbyrne@gmail.com>
2026-03-18 10:36:31 +0000

    fix(shell): set EDITOR to (n)vi(m) even if system setting present

--- dot_shell/env_static.sh
+++ dot_shell/env_static.sh
@@ -1,25 +1,21 @@
 # shellcheck shell=sh
 
-if [ -z "${EDITOR}" ]; then
-	if [ -x /snap/bin/nvim ]; then
-		EDITOR=/snap/bin/nvim
-	elif command -v nvim >/dev/null 2>&1; then
-		EDITOR=nvim
-	elif command -v vim >/dev/null 2>&1; then
-		EDITOR=vim
-	else
-		EDITOR=vi
-	fi
+export PNPM_HOME="$HOME/.local/share/pnpm"
+export WORKSPACE_HOME="$HOME/tmp/workspaces"
+export PATH="$HOME/.local/bin:$PNPM_HOME:/home/linuxbrew/.linuxbrew/bin:/usr/local/bin:/usr/bin:/bin:/snap/bin${PATH:+:$PATH}"
+
+if command -v nvim >/dev/null 2>&1; then
+	EDITOR="$(command -v nvim)"
+elif command -v vim >/dev/null 2>&1; then
+	EDITOR="$(command -v vim)"
+else
+	EDITOR="$(command -v vi)"
 fi
 export EDITOR
 
-if [ -z "${VISUAL+x}" ]; then
-	VISUAL=$EDITOR
+if [ -z "${VISUAL:-}" ]; then
+	VISUAL="$EDITOR"
 fi
 export VISUAL
+
 export PAGER=less
-
-export PNPM_HOME="$HOME/.local/share/pnpm"
-export WORKSPACE_HOME="$HOME/tmp/workspaces"
-
-export PATH="$HOME/.local/bin:$PNPM_HOME:/home/linuxbrew/.linuxbrew/bin:/usr/local/bin:/usr/bin:/bin:/snap/bin${PATH:+:$PATH}"
