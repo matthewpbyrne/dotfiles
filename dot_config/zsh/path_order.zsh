01be1f94dafce607937f4dbdabfa308c441066ac
Matthew Byrne <matthewpbyrne@gmail.com>
2026-03-20 02:49:00 +0000

    fixup! refactor(zsh): path handling

--- dot_config/zsh/path_order.zsh
+++ dot_config/zsh/path_order.zsh
@@ -2,20 +2,21 @@
 typeset -U path
 
 path=(
-  "$HOME/.local/bin"
-  "$HOME/.cargo/bin"
-  "$HOME/.cabal/bin"
-  "$HOME/.ghcup/bin"
-  "$HOME/.luarocks/bin"
-  "${PNPM_HOME:-$HOME/.local/share/pnpm}"
-  "${GOBIN:-}"
-  "$HOME/.asdf/shims"
-  "/home/linuxbrew/.linuxbrew/bin"
-  "/usr/local/bin"
-  "/usr/bin"
-  "/bin"
-  "/snap/bin"
-  $path
+	"$HOME/.local/bin"
+	"$HOME/.cargo/bin"
+	"$HOME/.cabal/bin"
+	"$HOME/.ghcup/bin"
+	"$HOME/.luarocks/bin"
+	"${PNPM_HOME:-$HOME/.local/share/pnpm}"
+	"${GOBIN:-}"
+	"${ASDF_DATA_DIR:-$HOME/.asdf}/shims"
+	"${ASDF_DATA_DIR:-$HOME/.asdf}/bin"
+	"/home/linuxbrew/.linuxbrew/bin"
+	"/usr/local/bin"
+	"/usr/bin"
+	"/bin"
+	"/snap/bin"
+	$path
 )
 
 path=(${path:#""})
